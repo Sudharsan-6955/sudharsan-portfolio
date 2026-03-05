2b27b8261105e2213ee801eb3ec9d61e08ddd430
Sudharsan-6955 <sudharsan6955@gmail.com>
2026-03-05 23:31:13 +0530

    chnagine fonts

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -5,3 +5,15 @@ import ContactPage from '@/components/contact/ContactPage';
 export default function Contact() {
   return <ContactPage />;
 }
+
+
+// git checkout dev
+// # do changes
+// git add .
+// git commit -m "Update portfolio section"
+// git push origin dev
+
+// # when ready
+// git checkout main
+// git merge dev
+// git push origin main
